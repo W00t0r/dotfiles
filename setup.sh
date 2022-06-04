c84f3eedf9f45e1860e516e4a11e9e2875ce453f
Wouter van Ackooy <wouter.v.ackooy@gmail.com>
2022-06-04 10:15:19 +0200

    Alias should be at then end, oops. Added timestamps to bak files

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env zsh
 
-if test -f "~/.zshrc"; then
+timestamp=$(date +%Y-%m-%d_%H.%M.%S)
+
+if [ ! -f "~/.zshrc" ]; then
+    cp ~/.zshrc ./zsh/.zshrc.bak.$timestamp
+    rm ~/.zshrc
     ln -s $PWD/zsh/.zshrc ~/.zshrc
 fi
 
-if test -f "~/.oh-my-zsh/themes/w00t0r.zsh-theme"; then
+if [ ! -f "~/.oh-my-zsh/themes/w00t0r.zsh-theme" ]; then
+    cp ~/.oh-my-zsh/themes/w00t0r.zsh-theme ./zsh/w00t0r.zsh-theme.bak.$timestamp
+    rm ~/.oh-my-zsh/themes/w00t0r.zsh-theme
     ln -s $PWD/zsh/w00t0r.zsh-theme ~/.oh-my-zsh/themes
 fi
 
-if test -f "~/.vimrc"; then
+if [ ! -f "~/.vimrc" ]; then
+    cp ~/.vimrc ./vim/.vimrc.bak.$timestamp
+    rm ~/.vimrc
     ln -s $PWD/vim/.vimrc ~/.vimrc
 fi
 
-if test -f "~/.config/nvim/init.vim"; then
+if [ ! -f "~/.config/nvim/init.vim" ]; then
+    rm ~/.config/nvim/init.vim
     ln -s $PWD/vim/.vimrc ~/.config/nvim/init.vim
 fi
+
+source ~/.zshrc
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -7,16 +7,17 @@ CASE_SENSITIVE="true"
 HIST_STAMPS="yyyy-mm-dd"
 plugins=(git)
 
+source $ZSH/oh-my-zsh.sh
+
 # Aliases
 alias ls='ls --color=auto'
+alias ll='ls -Al --color=auto'
 alias lh='ls -hl --color=auto'
 alias la='ls -hAl --color=auto'
 alias grep='grep --color=auto'
 
 alias zshrc='vim $HOME/.zshrc'
 alias vimrc='vim $HOME/.vimrc'
-
-source $ZSH/oh-my-zsh.sh
 
 alias node-docker='docker run -ti -v "$PWD":/usr/src/app -w /usr/src/app -p 3000:3000 node:14'
 alias vim='nvim'
